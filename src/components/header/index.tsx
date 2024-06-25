cf62db7450248bd54cbee48382a61afdf286e17b
Lucas Bastos <lucsbasto@gmail.com>
2024-06-25 19:19:39 -0300

    refactor: change appbar style

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,5 +1,5 @@
 import { colors } from '@/theme/colors';
-import { Ionicons } from "@expo/vector-icons";
+import { MaterialIcons, Octicons } from "@expo/vector-icons";
 import React from 'react';
 import { Image, SafeAreaView, StyleSheet, Text, View } from 'react-native';
 
@@ -11,17 +11,15 @@ export default function Header() {
             source={{uri: "https://github.com/lucsbasto.png"}}
             style={styles.image}
           />
-          <View style={{flex: 1, marginTop: 50, marginLeft: 20}}>
-            <Text style={{color: "red", fontSize: 15 }}>
-              Welcome
-            </Text>
-            <Text  style={{color: colors.white, fontSize: 20, fontWeight: 'bold'}}>
-              Lucas Bastos
-            </Text>
+          <View style={{flex: 1, marginTop: 20, marginLeft: 20}}>
+            <View style={{display: 'flex', flexDirection: 'row', alignItems: 'center'}}>
+              <Text style={{color: colors.stone[700], fontSize: 20, fontWeight: 'bold', marginEnd: 10 }}>
+                Lucas Bastos
+              </Text>
+              <Octicons name="verified" size={24} color="silver" />
+            </View>
           </View>
-          <View style={styles.notification}>
-            <Ionicons name="notifications" size={30} color={colors.gray[700]}/>
-          </View>
+          <MaterialIcons name="logout" size={24} color="black" style={{marginTop: 20, marginRight: 10}}/>
         </View>
 
     </SafeAreaView>
@@ -29,35 +27,24 @@ export default function Header() {
 }
 const styles = StyleSheet.create({
   image: {
-    marginTop: 50,
     width: 60,
     height: 60,
     borderRadius: 50,
-    marginLeft: 20
+    marginLeft: 20,
+    marginTop: 20,
   },
   container: {
     width: '100%',
-    height: '20%',
-    marginTop: 0,
-    backgroundColor: colors.primary,
-    borderBottomLeftRadius: 25,
-    borderBottomRightRadius: 25,
+    height: 100,
+    backgroundColor: colors.stone[100],
+    borderBottomLeftRadius: 30,
+    borderBottomRightRadius: 30,
   },
   profile: {
+    flex: 1,
+    marginBottom: 0,
     display: 'flex',
     flexDirection: 'row',
     alignItems: 'center',
-    padding: 10
-  },
-  notification: {
-    borderRadius: 25, 
-    backgroundColor: '#cecece', 
-    height: 40, 
-    width: 40,
-     display: 'flex', 
-     alignItems: 'center', 
-     justifyContent: 'center',
-     marginTop: 50,
-     marginRight: 20
-    }
+  }
 })
